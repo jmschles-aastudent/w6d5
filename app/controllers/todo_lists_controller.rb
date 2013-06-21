5a6aa4a2426b0fe497669887fe1fa2619a1ea906
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-06-21 14:14:19 -0700

    add new lists dynamically

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -2,14 +2,21 @@ class TodoListsController < ApplicationController
 
 	def index
 		@todo_lists = TodoList.all
+		@todo_list = TodoList.new
 	end
 
 	def create
 		@todo_list = TodoList.new(params[:todo_list])
-		if @todo_list.save
-			redirect_to todo_lists_url
-		else
-			render :new
+		@todo_list.save!
+		# if @todo_list.save
+		# 	redirect_to todo_lists_url
+		# else
+		# 	render :new
+		# end
+
+		respond_to do |format|
+			format.html { render :index }
+			format.json { render :json => @todo_list }
 		end
 	end
 
