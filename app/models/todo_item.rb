74dd081bbce1d1d7e4a64cbcf49fab1d86c743ee
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-06-21 10:57:10 -0700

    add validations to TodoList and TodoItem

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,5 +1,7 @@
 class TodoItem < ActiveRecord::Base
   attr_accessible :list_id, :name
 
+  validates :list, :name, :presence => true
+
   belongs_to :list, :class_name => "TodoList"
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,7 @@
 class TodoList < ActiveRecord::Base
   attr_accessible :title
 
+  validates :title, :presence => true
+
   has_many :items, :class_name => "TodoItem", :foreign_key => "list_id"
 end
