5504d10e7669de98202cc54d1e467cfe38db8158
Jamie Schlessinger <james.schlessinger@gmail.com>
2013-06-21 11:59:46 -0700

    show and hide list items

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -3,5 +3,8 @@ class TodoList < ActiveRecord::Base
 
   validates :title, :presence => true
 
-  has_many :items, :class_name => "TodoItem", :foreign_key => "list_id"
+  has_many :items,
+  	:class_name => "TodoItem",
+  	:foreign_key => "list_id",
+  	:inverse_of => :list
 end
